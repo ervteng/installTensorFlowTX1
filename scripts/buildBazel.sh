9a9a0c65b679be332b78b4ebf9e54f37c1ccb887
Ervin Teng <ervin.teng@sv.cmu.edu>
2017-05-10 01:48:57 +0000

    Update to Bazel 0.4.5

--- scripts/buildBazel.sh
+++ scripts/buildBazel.sh
@@ -5,11 +5,11 @@
 INSTALL_DIR=$PWD
 cd $HOME
 # zip in release 0.4.3 includes working arm32 binaries for bootstrapping
-wget https://github.com/bazelbuild/bazel/releases/download/0.4.3/bazel-0.4.3-dist.zip --no-check-certificate
-mkdir bazel-0.4.3
-cd bazel-0.4.3/
-unzip ../bazel-0.4.3-dist.zip
+wget https://github.com/bazelbuild/bazel/releases/download/0.4.5/bazel-0.4.5-dist.zip --no-check-certificate
+mkdir bazel-0.4.5
+cd bazel-0.4.5/
+unzip ../bazel-0.4.5-dist.zip
 # yet bazel needs to be patched to compile (and run correctly) on aarch64
-patch -p1 <$INSTALL_DIR/patches/bazel-0.4.3-arm64.patch
+patch -p1 <$INSTALL_DIR/patches/bazel-0.4.5-arm64.patch
 ./compile.sh
 sudo cp output/bazel /usr/local/bin
